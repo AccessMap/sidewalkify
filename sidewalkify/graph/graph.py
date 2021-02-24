c6b488d5cf181ba0f1cac5733a45dc59c3c658e4
= <=>
2021-02-23 16:03:59 -0800

    Flattened function definitions in graph module

--- sidewalkify/graph/graph.py
+++ sidewalkify/graph/graph.py
@@ -17,50 +17,8 @@ def create_graph(gdf, precision=1, simplify=0.05):
     """
     # The geometries sometimes have tiny end parts - get rid of those!
     gdf.geometry = gdf.geometry.simplify(simplify)
-
     G = nx.DiGraph()
-
-    # TODO: converting to string is probably unnecessary - keeping float may be
-    # faster
-    def make_node(coord, precision):
-        return tuple(np.round(coord, precision))
-
-    # Edges are stored as (from, to, data), where from and to are nodes.
-    # az1 is the azimuth of the first segment of the geometry (point into the
-    # geometry), az2 is for the last segment (pointing out of the geometry)
-    def add_edges(row, G):
-        geom = row.geometry
-        coords = list(geom.coords)
-        geom_r = geometry.LineString(coords[::-1])
-        coords_r = geom_r.coords
-        start = make_node(coords[0], precision)
-        end = make_node(coords[-1], precision)
-
-        # Add forward edge
-        fwd_attr = {
-            "forward": 1,
-            "geometry": geom,
-            "az1": azimuth(coords[0], coords[1]),
-            "az2": azimuth(coords[-2], coords[-1]),
-            "offset": row.sw_left,
-            "visited": 0,
-            "id": row.id,
-        }
-        G.add_edge(start, end, **fwd_attr)
-
-        # Add reverse edge
-        rev_attr = {
-            "forward": 0,
-            "geometry": geom_r,
-            "az1": azimuth(coords_r[0], coords_r[1]),
-            "az2": azimuth(coords_r[-2], coords_r[-1]),
-            "offset": row.sw_right,
-            "visited": 0,
-            "id": row.id,
-        }
-        G.add_edge(end, start, **rev_attr)
-
-    gdf.apply(add_edges, axis=1, args=[G])
+    gdf.apply(add_edges, axis=1, args=[G, precision])
 
     return G
 
@@ -114,13 +72,6 @@ def find_path(G, u, v):
     path["nodes"].append(u)
     path["nodes"].append(v)
 
-    def circular_dist(G, u, v, x):
-        if u == x:
-            # Should sort last - just make it a big int
-            return 1e6
-        else:
-            return cw_distance((G[u][v]["az2"] + 180) % 360, G[v][x]["az1"])
-
     while True:
         u_previous = u
         u = v
@@ -145,8 +96,58 @@ def find_path(G, u, v):
     return path
 
 
+# TODO: converting to string is probably unnecessary - keeping float may be
+# faster
+def make_node(coord, precision):
+    return tuple(np.round(coord, precision))
+
+
+# Edges are stored as (from, to, data), where from and to are nodes.
+# az1 is the azimuth of the first segment of the geometry (point into the
+# geometry), az2 is for the last segment (pointing out of the geometry)
+def add_edges(row, G, precision):
+    geom = row.geometry
+    coords = list(geom.coords)
+    geom_r = geometry.LineString(coords[::-1])
+    coords_r = geom_r.coords
+    start = make_node(coords[0], precision)
+    end = make_node(coords[-1], precision)
+
+    # Add forward edge
+    fwd_attr = {
+        "forward": 1,
+        "geometry": geom,
+        "az1": azimuth(coords[0], coords[1]),
+        "az2": azimuth(coords[-2], coords[-1]),
+        "offset": row.sw_left,
+        "visited": 0,
+        "id": row.id,
+    }
+    G.add_edge(start, end, **fwd_attr)
+
+    # Add reverse edge
+    rev_attr = {
+        "forward": 0,
+        "geometry": geom_r,
+        "az1": azimuth(coords_r[0], coords_r[1]),
+        "az2": azimuth(coords_r[-2], coords_r[-1]),
+        "offset": row.sw_right,
+        "visited": 0,
+        "id": row.id,
+    }
+    G.add_edge(end, start, **rev_attr)
+
+
 def graph_workflow(gdf, precision=1):
     G = create_graph(gdf, precision=precision)
     paths = find_paths(G)
 
     return paths
+
+
+def circular_dist(G, u, v, x):
+    if u == x:
+        # Should sort last - just make it a big int
+        return 1e6
+    else:
+        return cw_distance((G[u][v]["az2"] + 180) % 360, G[v][x]["az1"])
